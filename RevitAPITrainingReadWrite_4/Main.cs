38131d4f27a512dcf7b256c399c7a1cda0c1dafd
Татьяна <veselovskayt@mail.ru>
2022-03-16 11:43:41 +0700

    Добавьте файлы проекта.

---
+++ RevitAPITrainingReadWrite_4/Main.cs
@@ -0,0 +1,62 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Microsoft.Win32;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenFileDialog = System.Windows.Forms.OpenFileDialog;
+using SaveFileDialog = System.Windows.Forms.SaveFileDialog;
+
+namespace RevitAPITrainingReadWrite_4
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            string roomInfo = string.Empty;
+
+            var rooms = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_Rooms)
+                .Cast<Room>()
+                .ToList();
+
+            string excelPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "rooms.xlsx");
+            // сохранение файла на рабочем столе, название файла
+            using (FileStream stream = new FileStream(excelPath, FileMode.Create, FileAccess.Write))// создание файла
+            {
+                IWorkbook workbook = new HSSFWorkbook(); // создание книги
+                ISheet sheet = workbook.CreateSheet("Лист1"); // создание листа
+
+                int rowIndex = 0;                       //1 строка
+                foreach (var room in rooms)             // проходимся по каждому помещению в модели
+                {
+                    sheet.SetCellValue(rowIndex, columnIndex: 0, room.Name); // записываю 1 значение в 1 столбец
+                    sheet.SetCellValue(rowIndex, columnIndex: 1, room.Number); // 2 столбец
+                    sheet.SetCellValue(rowIndex, columnIndex: 2, room.Area); // 3 столбец
+                    rowIndex++;                         //переход на следующую строку
+                }
+
+                workbook.Write(stream);                 // закрываем запись в ехселе
+                workbook.Close();
+            }
+
+            System.Diagnostics.Process.Start(excelPath); // запускаем файл ехселя автоматически
+
+            return Result.Succeeded;
+        }
+    }
+}
+
---
+++ RevitAPITrainingReadWrite_4/SheetExts.cs
@@ -0,0 +1,40 @@
+﻿using NPOI.SS.UserModel;
+using NPOI.SS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITrainingReadWrite_4
+{
+    public static class SheetExts
+    {
+        //метод для заполнения листа даннными
+        public static void SetCellValue<T>(this ISheet sheet, int rowIndex, int columnIndex, T value)
+        // адрес ячейки: номер строки, номер столбца, запись значения
+        {
+            var cellReference = new CellReference(rowIndex, columnIndex);// ссылка на ячейку
+            var row = sheet.GetRow(cellReference.Row); // проверяем наличме строки
+            if (row == null)                            // если строки нет
+                row = sheet.CreateRow(cellReference.Row); // создаем 
+
+            var cell = row.GetCell(cellReference.Col); // создаем ссылку на ячейку и в выбранной строке, конкренная ячейка
+            if (cell == null)                        // если ячейка пустая
+                cell = row.CreateCell(cellReference.Col); // создаем 
+
+            if (value is string) //если передан текст то записываем данное значение как текст
+            {
+                cell.SetCellValue((string)(object)value); // делаем преобразование в object потом в string, на прямую нельзя
+            }
+            else if (value is double) // если значение double
+            {
+                cell.SetCellValue((double)(object)value);
+            }
+            else if (value is int)  //если значение целочисленное
+            {
+                cell.SetCellValue((int)(object)value);
+            }
+        }    
+    }
+}
